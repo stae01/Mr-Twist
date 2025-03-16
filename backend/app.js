4a21f9d3e93ae901f2c554ae5b0546b7dffa71fb
stae01 <166432863+stae01@users.noreply.github.com>
2025-03-16 16:04:03 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,20 @@
-const express = require('express')
+const express = require('express');
 
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hola');
+});
+
+app.listen(PORT, () => {
+
+    console.log('Servidor escuchando enb el puerto 3000');
+
+});
